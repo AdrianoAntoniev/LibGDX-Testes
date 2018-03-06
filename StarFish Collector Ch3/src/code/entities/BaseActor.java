7798d81c1109451ebf8403fdb2b8288644298814
Adriano <adriano.antoniev@outlook.com>
2018-03-05 23:34:08 -0300

    Commit inicial

---
+++ StarFish Collector Ch3/src/code/entities/BaseActor.java
@@ -0,0 +1,12 @@
+package code.entities;
+
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+
+public class BaseActor extends Actor {
+	public BaseActor(float x, float y, Stage s) {
+		super();
+		setPosition(x, y);
+		s.addActor(this);
+	}
+}
