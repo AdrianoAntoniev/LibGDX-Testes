be44271fcc672e1cfaae136fff001c5719faccd1
Adriano <adriano.antoniev@outlook.com>
2018-03-07 10:51:54 -0300

    Commit inicial

---
+++ Starfish Collector Ch5/src/code/Starfish.java
@@ -0,0 +1,34 @@
+package code;
+
+import com.badlogic.gdx.scenes.scene2d.Action;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.actions.Actions;
+
+public class Starfish extends BaseActor {
+	private boolean collected;
+	
+	public Starfish(float x, float y, Stage s) {
+		super(x, y, s);
+		
+		collected = false;
+		loadTexture("assets/starfish.png");
+		
+		Action spin = Actions.rotateBy(30, 1);
+		this.addAction(Actions.forever(spin));
+		
+		setBoundaryPolygon(8);
+	}
+	
+	public boolean isCollected() {
+		return collected;
+	}
+	
+	public void collect() {
+		collected = true;
+		clearActions();
+		addAction(Actions.fadeOut(1));
+		addAction(Actions.after(Actions.removeActor()));
+
+	}
+
+}
---
+++ Starfish Collector Ch5/src/code/StarfishGame.java
@@ -0,0 +1,11 @@
+package code;
+
+public class StarfishGame extends BaseGame {
+
+	@Override
+	public void create() {
+		setActiveScreen(new MenuScreen());
+		
+	}
+
+}
