4542518aa9127bacbcffe50d8c2b0ec0e2673afd
Adriano <adriano.antoniev@outlook.com>
2018-03-06 23:56:34 -0300

    Estruturação para futuras inclusões de telas;
    inclusão de tela de título.

---
+++ StarFish Collector Ch3/src/code/BaseScreen.java
@@ -0,0 +1,52 @@
+package code;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+
+public abstract class BaseScreen implements Screen {
+	protected Stage mainStage;
+	protected Stage uiStage;
+	
+	public BaseScreen() {
+		mainStage = new Stage();
+		uiStage = new Stage();
+		
+		initialize();
+	}
+
+	public abstract void initialize();
+	public abstract void update(float dt);
+	
+	public void render(float dt) {
+		mainStage.act(dt);
+		uiStage.act(dt);
+		
+		update(dt);
+		Gdx.gl.glClearColor(0,0,0,1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		
+		mainStage.draw();
+		uiStage.draw();
+	}
+
+	@Override
+	public void dispose() {}
+
+	@Override
+	public void hide() {}
+
+	@Override
+	public void pause() {}
+
+	@Override
+	public void resize(int arg0, int arg1) {}
+
+	@Override
+	public void resume() {}
+
+	@Override
+	public void show() {}
+	
+}
