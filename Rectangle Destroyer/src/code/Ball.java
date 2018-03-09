fb59b0c7894ee3db7e876bf0977c85f4cf51bdae
Adriano <adriano.antoniev@outlook.com>
2018-03-09 00:20:42 -0300

    Criação de blocos e bola do jogo.

---
+++ Rectangle Destroyer/src/code/Ball.java
@@ -0,0 +1,44 @@
+package code;
+
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+
+public class Ball extends BaseActor {
+	private boolean paused;
+
+	public Ball(float x, float y, Stage s) {
+		super(x, y, s);
+		loadTexture("assets/ball.png");
+		setSpeed(400);
+		setMotionAngle(90);
+		setBoundaryPolygon(12);
+		setPaused(true);
+	}
+
+	public void setPaused(boolean b) {
+		paused = b;
+	}
+	
+	public boolean isPaused() {
+		return paused;
+	}
+	
+	@Override
+	public void act(float dt) {
+		super.act(dt);
+		
+		if(!isPaused()) {
+			setAcceleration(10);
+			accelerateAtAngle(270);
+			applyPhysics(dt);
+		}
+	}
+	
+	public void bounceOff(BaseActor other) {
+		Vector2 v = this.preventOverlap(other);
+		if(Math.abs(v.x) >= Math.abs(v.y))
+			this.velocityVec.x *= -1;
+		else
+			this.velocityVec.y *= 1;
+	}
+}
--- Rectangle Destroyer/src/code/LevelScreen.java
+++ Rectangle Destroyer/src/code/LevelScreen.java
@@ -4,17 +4,55 @@ import com.badlogic.gdx.Gdx;
 
 public class LevelScreen extends BaseScreen {
 	Paddle paddle;
+	Ball ball;
 	
 	public void initialize() {
 		BaseActor background = new BaseActor(0, 0, mainStage);
 		background.loadTexture("assets/space.png");
 		BaseActor.setWorldBounds(background);
 		paddle = new Paddle(320, 32, mainStage);
+		
+		new Wall(0, 0, 20, 600, mainStage);
+		new Wall(780, 0, 20, 600, mainStage);
+		new Wall(0, 550, 800, 50, mainStage);
+		
+		Brick tempBrick = new Brick(0,0,mainStage);
+		float brickWidth = tempBrick.getWidth();
+		float brickHeight = tempBrick.getHeight();
+		tempBrick.remove();
+		
+		int totalRows = 10;
+		int totalCols = 10;
+		float marginX = (800 - totalCols * brickWidth) / 2;
+		float marginY = (600 - totalRows * brickHeight) - 100;
+		
+		for (int rowNum=0; rowNum < totalRows; rowNum++) {
+			for (int colNum=0; colNum < totalCols; colNum++) {
+				float x = marginX + brickWidth * colNum;
+				float y = marginY + brickHeight * rowNum;
+				new Brick(x, y, mainStage);
+			}
+		}
+		
+		ball = new Ball(0,0, mainStage);
 	}
 
 	public void update(float dt) {
 		float mouseX = Gdx.input.getX();
 		paddle.setX(mouseX - paddle.getWidth()/2);
 		paddle.boundToWorld();
+		
+		if(ball.isPaused()) {
+			ball.setX(paddle.getX() + paddle.getWidth()/2 - ball.getWidth()/2);
+			ball.setY(paddle.getY() + paddle.getHeight()/2 - ball.getHeight()/2);
+		}
+	}
+	
+	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+		if(ball.isPaused()) {
+			ball.setPaused(false);
+		}
+		
+		return false;
 	}
 }
