c2d335f1236681a6232d1ef10f739241ec930e0f
Adriano <adriano.antoniev@outlook.com>
2018-03-11 01:22:02 -0300

    Commit inicial Jogo Rectangle Destroyer

---
+++ Rectangle Destroyer Ch10/src/code/Brick.java
@@ -0,0 +1,12 @@
+package code;
+
+import com.badlogic.gdx.scenes.scene2d.Stage;
+
+public class Brick extends BaseActor
+{
+    public Brick(float x, float y, Stage s)
+    {
+        super(x,y,s);
+        loadTexture("assets/brick-gray.png");
+    }
+}
---
+++ Rectangle Destroyer Ch10/src/code/RectangleGame.java
@@ -0,0 +1,10 @@
+package code;
+
+public class RectangleGame extends BaseGame
+{
+    public void create() 
+    {        
+        super.create();
+        setActiveScreen( new LevelScreen() );
+    }
+}
